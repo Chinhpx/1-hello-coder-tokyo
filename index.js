1a4c9fdf401d1ddd22248c98df7f3dad9b18b20e
unknown <chinh1995@gmail.com>
2021-05-05 20:48:47 +0700

    lesson15

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 var userRoute = require('./routes/user.route')
+var cookieParser = require('cookie-parser')
+var authRoute = require('./routes/auth.route')
 const port = 3000
 
 const app = express()
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser())
 
 app.set('view engine', 'pug')
 app.set('views', './views')
+
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -17,6 +21,7 @@ app.get('/', (req, res) => {
   })
 })
 
+app.use('/auth', authRoute)
 app.use('/users', userRoute)
 
 app.use(express.static('public'))
